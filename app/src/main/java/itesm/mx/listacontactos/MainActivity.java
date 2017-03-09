12a50b64005f6df21e20eedaed1a12d2b70c4cad
Hector <hectorortiz_96@hotmail.com>
2017-03-09 17:46:34 -0600

    First test Commit

---
+++ app/src/main/java/itesm/mx/listacontactos/MainActivity.java
@@ -0,0 +1,15 @@
+package itesm.mx.listacontactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Test for repo. If you see this it works
+    }
+}
