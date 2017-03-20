06e05a78df10b38b5c7f6b5a69143ec4223b2730
Alejandro Salgado <a.salgado.gaspar@gmail.com>
2017-03-20 11:32:39 -0600

    Mis cambios - Salgado

--- app/src/main/java/itesm/mx/listacontactos/MainActivity.java
+++ app/src/main/java/itesm/mx/listacontactos/MainActivity.java
@@ -1,13 +1,20 @@
 package itesm.mx.listacontactos;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +26,10 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
     List<String> listDataHeader;
     HashMap<String, List<String>> listDataChild;
     ImageButton buttonSettings;
+    List<Contacto> listaContactos;
+    final int REQUEST_PERMISOS = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,36 +66,128 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
     private void prepareListData() {
         listDataHeader = new ArrayList<>();
         listDataChild = new HashMap<String, List<String>>();
+        List<String> familiares =  new ArrayList<String>();
+        List<String> amigos =  new ArrayList<String>();
+        List<String> emergencias = new ArrayList<String>() ;
+        listaContactos = new ArrayList<Contacto>();
+
+
+        //Crea (si aun no ha sido creado) y accede al archivo donde esta guardada la informacion de los contactos
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+
+
+        //Guarda una contraseña
+        SharedPreferences.Editor editor = sharedPref.edit();
+        //editor.clear();
+        //editor.commit();
+        String passwordValor = "usuario";
+        editor.putString("password", passwordValor);
+        editor.commit();
+
+        //Trae la informacion de los contactos (se guardo en forma de un arreglo, la adquiero en forma de un arreglo)
+        Gson gson = new Gson();
+        String json = sharedPref.getString("Contactos", null);
+
+        Type listType = new TypeToken<ArrayList<Contacto>>(){}.getType();
+        listaContactos = gson.fromJson(json, listType);
+
+        if (listaContactos != null){
+            VariablesGlobales globalListaContactos = ((VariablesGlobales) getApplicationContext());
+            globalListaContactos.setListaContactos(listaContactos);
+
+            //Adding child data
+            for (int iCont = 0; iCont < listaContactos.size(); iCont++){
+                Contacto contactoTemp = listaContactos.get(iCont);
+                Log.d("Nombre:",listaContactos.get(iCont).getNombre());
+                if (contactoTemp.getTipo() == 0)
+                    familiares.add(contactoTemp.getNombre());
+                else if (contactoTemp.getTipo() == 1)
+                    amigos.add(contactoTemp.getNombre());
+                else if (contactoTemp.getTipo() == 2)
+                    emergencias.add(contactoTemp.getNombre());
+            }
+        }
+        else
+        {
+            List<Contacto> NuevaLista = new ArrayList<Contacto>();
+            VariablesGlobales globalListaContactos = ((VariablesGlobales) getApplicationContext());
+            globalListaContactos.setListaContactos(NuevaLista);
+        }
+
 
         // Adding child data
         listDataHeader.add("Familiares");
         listDataHeader.add("Amigos");
         listDataHeader.add("Servicios de emergencias");
 
+        /*
         // Adding child data
-        List<String> familiares = new ArrayList<String>();
-        familiares.add("The Shawshank Redemption");
-        familiares.add("The Godfather");
+        familiares.add("Alejandro Salgado");
+        familiares.add("Oscar Uriel");
 
-        List<String> amigos = new ArrayList<String>();
-        amigos.add("Pedro Martinex");
+        amigos.add("Carlos Sanchez");
         amigos.add("Martin Murillo");
 
-        List<String> emergencias = new ArrayList<String>();
         emergencias.add("Hospital Cruz Roja");
         emergencias.add("Policia");
-        emergencias.add("Atencion Adulto Mayor");
+        emergencias.add("Atencion Adulto Mayor");*/
 
         listDataChild.put(listDataHeader.get(0), familiares); // Header, Child data
         listDataChild.put(listDataHeader.get(1), amigos);
         listDataChild.put(listDataHeader.get(2), emergencias);
+
     }
 
 
     @Override
     public void onClick(View v) {
         Intent intent;
-        intent = new Intent(MainActivity.this, SettingsActivity.class);
-        startActivity(intent);
+        intent = new Intent(MainActivity.this, PermisosActivity.class);
+        startActivityForResult(intent,REQUEST_PERMISOS);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null){
+            Boolean actualizar = data.getBooleanExtra("actualizar",false);
+            if (actualizar) {
+
+                VariablesGlobales globalListaContactos = ((VariablesGlobales) getApplicationContext());
+                listaContactos = globalListaContactos.getListaContactos();
+
+                List<String> familiares =  new ArrayList<String>();
+                List<String> amigos =  new ArrayList<String>();
+                List<String> emergencias = new ArrayList<String>() ;
+
+                for (int iCont = 0; iCont < listaContactos.size(); iCont++){
+                    Contacto contactoTemp = listaContactos.get(iCont);
+                    if (contactoTemp.getTipo() == 0)
+                        familiares.add(contactoTemp.getNombre());
+                    else if (contactoTemp.getTipo() == 1)
+                        amigos.add(contactoTemp.getNombre());
+                    else if (contactoTemp.getTipo() == 2)
+                        emergencias.add(contactoTemp.getNombre());
+                }
+                listDataChild.clear();
+
+                /*
+                familiares.add("Alejandro Salgado");
+                familiares.add("Oscar Uriel");
+
+                amigos.add("Carlos Sanchez");
+                amigos.add("Martin Murillo");
+
+                emergencias.add("Hospital Cruz Roja");
+                emergencias.add("Policia");
+                emergencias.add("Atencion Adulto Mayor");*/
+
+                listDataChild.put(listDataHeader.get(0), familiares); // Header, Child data
+                listDataChild.put(listDataHeader.get(1), amigos);
+                listDataChild.put(listDataHeader.get(2), emergencias);
+
+                listAdapter.notifyDataSetChanged();
+            }
+        }
+
     }
 }
--- app/src/main/java/itesm/mx/listacontactos/SettingsActivity.java
+++ app/src/main/java/itesm/mx/listacontactos/SettingsActivity.java
@@ -1,13 +1,57 @@
 package itesm.mx.listacontactos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final int REQUEST_AGREGAR = 0;
+    final int REQUEST_MODIFICAR = 1;
+    final int REQUEST_BORRAR = 2;
+    Button btnAgregarContacto;
+    Button btnModificarContacto;
+    Button btnBorrarContacto;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        intent = new Intent();
+
+        btnAgregarContacto = (Button) findViewById(R.id.buttonAdd);
+        btnModificarContacto =  (Button) findViewById(R.id.buttonEdit);
+        btnBorrarContacto =  (Button) findViewById(R.id.buttonDelete);
+
+        btnAgregarContacto.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_AGREGAR && resultCode == RESULT_OK) {
+            intent.putExtra("agregado", true);
+        }
+        else if (requestCode == REQUEST_MODIFICAR && resultCode == RESULT_OK){
+            intent.putExtra("modificado",true);
+        }
+        else if ((requestCode == REQUEST_BORRAR&& resultCode == RESULT_OK)){
+            intent.putExtra("eliminado",true);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v){
+        if (true) {
+            Intent intent = new Intent(this, AgregarActivity.class);
+            startActivityForResult(intent, REQUEST_AGREGAR);
+        }
     }
 }
